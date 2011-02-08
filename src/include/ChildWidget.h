f3fc3c2d3e75134d3c86a8b3990a8134ad5ca769
zdenop <zdenop@gmail.com>
2011-02-08 22:59:55 +0100

    small fixes and code indent

--- src/include/ChildWidget.h
+++ src/include/ChildWidget.h
@@ -1,46 +1,48 @@
 /**********************************************************************
- * File:        ChildWidget.h
- * Description: ChildWidget functions
- * Author:      Marcel Kolodziejczyk
- * Created:     2010-01-06
- *
- * (C) Copyright 2010, Marcel Kolodziejczyk
- * (C) Copyright 2011, Zdenko Podobny
- **
- ** Licensed under the Apache License, Version 2.0 (the "License");
- ** you may not use this file except in compliance with the License.
- ** You may obtain a copy of the License at
- **
- **    http://www.apache.org/licenses/LICENSE-2.0
- **
- ** Unless required by applicable law or agreed to in writing, software
- ** distributed under the License is distributed on an "AS IS" BASIS,
- ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- ** See the License for the specific language governing permissions and
- ** limitations under the License.
- *
- **********************************************************************/
+* File:        ChildWidget.h
+* Description: ChildWidget functions
+* Author:      Marcel Kolodziejczyk
+* Created:     2010-01-06
+*
+* (C) Copyright 2010, Marcel Kolodziejczyk
+* (C) Copyright 2011, Zdenko Podobny
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**    http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*
+**********************************************************************/
 
 #ifndef CHILDWIDGET_H_
 #define CHILDWIDGET_H_
 
+#include <QAbstractItemView>
+#include <QCloseEvent>
+#include <QDebug>
+#include <QDir>
+#include <QFileInfo>
+#include <QGraphicsItem>
+#include <QHeaderView>
+#include <QMessageBox>
+#include <QSplitter>
 #include <QStandardItemModel>
 #include <QTableView>
-#include <QAbstractItemView>
-#include <QHeaderView>
-#include <QtGui/QGraphicsScene>
-#include <QtGui/QGraphicsView>
-#include <QtGui/QGraphicsRectItem>
-#include <QtGui/QPixmap>
-#include <QFileInfo>
-#include <QDir>
+#include <QtCore/qmath.h>
 #include <QTextStream>
 #include <QtGui/QApplication>
-#include <QDebug>
-#include <QCloseEvent>
-#include <QSplitter>
-#include <QMessageBox>
-#include <QtCore/qmath.h>
+#include <QtGui/QGraphicsRectItem>
+#include <QtGui/QGraphicsScene>
+#include <QtGui/QGraphicsView>
+#include <QtGui/QPixmap>
+#include <QTransform>
 
 class QGraphicsScene;
 class QGraphicsView;
@@ -52,78 +54,84 @@ class QGraphicsRectItem;
 
 class ChildWidget : public QSplitter
 {
-  Q_OBJECT
+Q_OBJECT
 
 public:
-  ChildWidget(QWidget * parent = 0);
+ChildWidget(QWidget * parent = 0);
 
-  bool isModified() { return modified; }
-  bool isBoxSelected();
-  bool isBold();
-  bool isItalic();
-  bool isUnderLine();
+bool isModified()
+{
+  return modified;
+}
+bool isBoxSelected();
+bool isBold();
+bool isItalic();
+bool isUnderLine();
 
-  QString userFriendlyCurrentFile();
-  QString currentBoxFile();
-  QString canonicalImageFileName() { return imageFile; }
+QString userFriendlyCurrentFile();
+QString currentBoxFile();
+QString canonicalImageFileName()
+{
+  return imageFile;
+}
 
-  bool save(const QString &fileName);
-  bool loadImage(const QString &fileName);
-  bool loadBoxes(const QString &fileName);
-  void setSelectionRect();
-  void setBolded(bool v);
-  void setItalic(bool v);
-  void setUnderline(bool v);
-  void setZoom(float scale);
-  void zoomOriginal();
-  void zoomIn();
-  void zoomOut();
-  void zoomToSelection();
-  void zoomToFit();
-  void zoomToHeight();
-  void zoomToWidth();
-  void drawBoxes();
-  void deleteBoxes(const QList<QGraphicsItem*> &items);
-  void splitSymbol();
-  void joinSymbol();
-  void deleteSymbol();
+bool save(const QString &fileName);
+bool loadImage(const QString &fileName);
+bool loadBoxes(const QString &fileName);
+void setSelectionRect();
+void setBolded(bool v);
+void setItalic(bool v);
+void setUnderline(bool v);
+void setZoom(float scale);
+void zoomOriginal();
+void zoomIn();
+void zoomOut();
+void zoomToSelection();
+void zoomToFit();
+void zoomToHeight();
+void zoomToWidth();
+void drawBoxes();
+void deleteBoxes(const QList<QGraphicsItem*> &items);
+void splitSymbol();
+void joinSymbol();
+void deleteSymbol();
 
 private slots:
-  void documentWasModified();
-  void emitBoxChanged();
-  void drawSelectionRects();
+void documentWasModified();
+void emitBoxChanged();
+void drawSelectionRects();
 
 protected:
-  void closeEvent(QCloseEvent *event);
-  bool maybeSave();
-  void setCurrentImageFile(const QString &fileName);
-  void setCurrentBoxFile(const QString &fileName);
+void closeEvent(QCloseEvent *event);
+bool maybeSave();
+void setCurrentImageFile(const QString &fileName);
+void setCurrentBoxFile(const QString &fileName);
 
-  QString strippedName(const QString &fullFileName);
+QString strippedName(const QString &fullFileName);
 
 signals:
-  void boxChanged();
-  void modifiedChanged();
+void boxChanged();
+void modifiedChanged();
 
 protected:
-  QGraphicsScene *imageScene;
-  QGraphicsView *imageView;
-  QGraphicsItem * imageItem;
-  QGraphicsRectItem *imageSelectionRect;
+QGraphicsScene *imageScene;
+QGraphicsView *imageView;
+QGraphicsItem * imageItem;
+QGraphicsRectItem *imageSelectionRect;
 
-  QTableView *table;
+QTableView *table;
 
-  QAbstractItemModel *model;
-  QItemSelectionModel *selectionModel;
+QAbstractItemModel *model;
+QItemSelectionModel *selectionModel;
 
-  QString imageFile;
-  QString boxFile;
+QString imageFile;
+QString boxFile;
 
-  bool modified;
-  bool boxesVisible;
-  bool ToSelection;
-  int imageHeight;
-  int imageWidth;
+bool modified;
+bool boxesVisible;
+bool ToSelection;
+int imageHeight;
+int imageWidth;
 };
 
 #endif /* CHILDWIDGET_H_ */
--- src/include/Settings.h
+++ src/include/Settings.h
@@ -1,36 +1,36 @@
 /**********************************************************************
- * File:        main.cpp
- * Description: Main function
- * Author:      Zdenko Podobny
- * Created:     2011-01-20
- *
- * (C) Copyright 2011, Zdenko Podobny
- **
- ** Licensed under the Apache License, Version 2.0 (the "License");
- ** you may not use this file except in compliance with the License.
- ** You may obtain a copy of the License at
- **
- **    http://www.apache.org/licenses/LICENSE-2.0
- **
- ** Unless required by applicable law or agreed to in writing, software
- ** distributed under the License is distributed on an "AS IS" BASIS,
- ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- ** See the License for the specific language governing permissions and
- ** limitations under the License.
- *
- **********************************************************************/
+* File:        main.cpp
+* Description: Main function
+* Author:      Zdenko Podobny
+* Created:     2011-01-20
+*
+* (C) Copyright 2011, Zdenko Podobny
+**
+** Licensed under the Apache License, Version 2.0 (the "License");
+** you may not use this file except in compliance with the License.
+** You may obtain a copy of the License at
+**
+**    http://www.apache.org/licenses/LICENSE-2.0
+**
+** Unless required by applicable law or agreed to in writing, software
+** distributed under the License is distributed on an "AS IS" BASIS,
+** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+** See the License for the specific language governing permissions and
+** limitations under the License.
+*
+**********************************************************************/
 
 #ifndef SETTINGS_H
 #define SETTINGS_H
 
-#define SETTING_ORGANIZATION "qt-box-editor"
-#define SETTING_APPLICATION "QT Box Editor"
+#define ORGANIZATION_NAME "qt-box-editor"
+#define APPLICATION_NAME "QT Box Editor"
 
 #define UPDATE_URL "https://github.com/zdenop/qt-box-editor/raw/master/CURRENT_VERSION"
 #define PROJECT_URL "http://github.com/zdenop/qt-box-editor"
 #define PROJECT_URL_NAME "github.com/zdenop/qt-box-editor"
-#define TABLE_FONT "Aegyptus"
-#define TABLE_FONT_SIZE 20
+#define TABLE_FONT "Sans"
+#define TABLE_FONT_SIZE 11
 #define DRAWBOX_COLOR QPen(Qt::green)
 #define SELECTED_BOX_COLOR QPen(Qt::red)
 
